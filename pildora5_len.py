4170790e83ad5712149dc4b7c2abf5c034d07bd1
PythonPill <87998428+PythonPill@users.noreply.github.com>
2021-08-31 20:16:18 +0200

    Add files via upload

---
+++ pildora5_len.py
@@ -0,0 +1,4 @@
+lista=[1,2,"a","b"]
+print(len(lista))
+palabra="perro"
+print(len(palabra))
---
+++ pildora6_input.py
@@ -0,0 +1,4 @@
+palabra=input("Inserta una palabra: ")
+print("La palabra insertada es: "+palabra)
+numero=int(input("Inserta un numero: "))
+print("El numero insertado es: "+str(numero))
