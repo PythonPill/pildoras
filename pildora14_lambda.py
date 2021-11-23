19a0d55198549e5726e451f251fd8ebaa98d8a7e
PythonPill <87998428+PythonPill@users.noreply.github.com>
2021-11-23 20:01:29 +0100

    Add files via upload

---
+++ pildora14_lambda.py
@@ -0,0 +1,11 @@
+#resultado=lambda x:x+10
+#print(resultado(5))
+#
+#resultado=lambda a,b:a+b
+#print(resultado(10,4))
+#
+resultado=lambda x: True if (x%2==0) else False
+if resultado(2):
+	print("el numero es par")
+else:
+	print("el numero es impar")
