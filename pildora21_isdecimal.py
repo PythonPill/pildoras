c12ffbe938444ca2b5acb12407cb630604e30c87
PythonPill <87998428+PythonPill@users.noreply.github.com>
2022-02-06 13:50:54 +0100

    Add files via upload

---
+++ pildora21_isdecimal.py
@@ -0,0 +1,6 @@
+var1="hola"
+#print(var1.isdecimal())
+if var1.isdecimal():
+	print ("La variable es alfanumerica y contiene solo numeros")
+else:
+	print ("La variable es alfanumerica y no contiene solo numeros")
